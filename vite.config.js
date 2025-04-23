56aca95af0da79557537755479106308d41a184f
nabildzr <muhammadnbillz@gmail.com>
2025-04-23 07:52:20 +0700

    Remove unnecessary comment from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import dotenv from 'dotenv'
 
-// https://vite.dev/config/
 dotenv.config()
 
 export default defineConfig({
