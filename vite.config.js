c9240b2ffde0d3a22ae04195856674593a980aca
nabildzr <muhammadnbillz@gmail.com>
2025-04-23 07:52:07 +0700

    Add dotenv dependency and configure environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin:true,
+        target: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
+        changeOrigin: true,
         headers: {
           Accept: 'application/json',
           "Content-Type": 'application/json'
