263ee3db2fab4d49d1154bb02ba03d6e10b95ec7
nabildzr <muhammadnbillz@gmail.com>
2025-04-23 08:28:22 +0700

    Add VITE_API_BASE_URL to .env, update .gitignore to include .env, and refactor vite.config.js to use environment variable for API base URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router";
+import Layout from "./pages/Layout";
 import Home from "./pages/Home/Home";
-import Layout from "./pages/layout";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import { AppContext } from "./context/AppContext";
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import dotenv from 'dotenv'
 
-dotenv.config()
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
-        changeOrigin: true,
+        target: `https://react-fullstack-api-be-b67713e657b5.herokuapp.com`,
+        changeOrigin:true,
         headers: {
           Accept: 'application/json',
           "Content-Type": 'application/json'
